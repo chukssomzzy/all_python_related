e2ed6b4576c9006415907edd0d62746cd52ef852
chukssomzzy <chukssomzzy19@gmail.com>
2023-05-11 09:11:01 +0000

    compose

---
+++ composition/compose/contacts.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+""" Describes an employees address """
+
+class Address:
+    def __init__(self, street, city, state, zipcode, street2=''):
+        self.street = street
+        self.street2 = street2
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+
+    def __str__(self):
+        """ What is printed by when address in passed as arg to print"""
+        lines = [self.street]
+        if self.street2:
+            lines.append(self.street2)
+        lines.append(f'{self.city}, {self.state} {self.zipcode}')
+        return '\n'.join(lines)
