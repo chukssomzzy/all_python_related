c039b631806c35d32f7e4c5d86c8c02fccba2161
chukssomzzy <chukssomzzy19@gmail.com>
2023-05-12 15:08:27 +0000

    notho

--- composition/compose/employees.py
+++ composition/compose/employees.py
@@ -1,50 +1,67 @@
 #!/usr/bin/env python3
 
-from hr import (
-    SalaryPolicy,
-    CommissionPolicy,
-    HourlyPolicy
-)
-from productivity import (
-    ManagerRole,
-    SecretaryRole,
-    SalesRole,
-    FactoryRole
-)
+from hr import get_policy
+from productivity import get_role
+from contacts import get_employee_address
+from representation import AsDictionaryMixin
 
 
-class Employee:
-    def __init__(self, id, name):
+class _EmployeeDatabase:
+
+    def __init__(self):
+        self._employees = {
+            1: {
+                'name': 'Mary Poppins',
+                'role': 'manager'
+            },
+            2: {
+                'name': 'John Smith',
+                'role': 'secretary'
+            },
+            3: {
+                'name': 'Kevin Bacon',
+                'role': 'sales'
+            },
+            4: {
+                'name': 'Jane Doe',
+                'role': 'factory'
+            },
+            5: {
+                'name': 'Robin Williams',
+                'role': 'secretary'
+            }
+        }
+
+
+    @property
+    def employees(self):
+        return [Employee(id_) for id_ in sorted(self._employees)]
+
+    def get_employee_info(self, employee_id):
+        info = self._employees.get(employee_id)
+        if not info:
+            raise ValueError(employee_id)
+        return info
+
+
+class Employee(AsDictionaryMixin):
+    def __init__(self, id):
         self.id = id
-        self.name = name
-        self.address = None
+        info = employee_database.get_employee_info(self.id)
+        self.name = info.get('name')
+        self.address = get_employee_address(self.id)
+        self._role = get_role(info.get('role'))
+        self._payroll = get_policy(self.id)
+
+    def work(self, hours):
+        duties = self._role.perform_duties(hours)
+        print(f'Employee {self.id} - {self.name}:')
+        print(f'- {duties}')
+        print('')
+        self._payroll.track_work(hours)
+
+    def calculate_payroll(self):
+        return self._payroll.calculate_payroll()
 
 
-class Manager(Employee, ManagerRole, SalaryPolicy):
-    def __init__(self, id, name, weekly_salary):
-        SalaryPolicy.__init__(self, weekly_salary)
-        super().__init__(id, name)
-
-
-class Secretary(Employee, SecretaryRole, SalaryPolicy):
-    def __init__(self, id, name, weekly_salary):
-        SalaryPolicy.__init__(self, weekly_salary)
-        super().__init__(id, name)
-
-
-class SalesPerson(Employee, SalesRole, CommissionPolicy):
-    def __init__(self, id, name, weekly_salary, commission):
-        CommissionPolicy.__init__(self, weekly_salary, commission)
-        super().__init__(id, name)
-
-
-class FactoryWorker(Employee, FactoryRole, HourlyPolicy):
-    def __init__(self, id, name, hours_worked, hour_rate):
-        HourlyPolicy.__init__(self, hours_worked, hour_rate)
-        super().__init__(id, name)
-
-
-class TemporarySecretary(Employee, SecretaryRole, HourlyPolicy):
-    def __init__(self, id, name, hours_worked, hour_rate):
-        HourlyPolicy.__init__(self, hours_worked, hour_rate)
-        super().__init__(id, name)
+employee_database = _EmployeeDatabase()
--- composition/compose/productivity.py
+++ composition/compose/productivity.py
@@ -2,7 +2,21 @@
 
 """ defines the implementaion for the productivity system"""
 
-class ProductivitySystem:
+class _ProductivitySystem:
+    def __init__(self):
+        self._roles = {
+            'manager': ManagerRole,
+            'secretary': SecretaryRole,
+            'sales': SalesRole,
+            'factory': FactoryRole
+        }
+
+    def get_role(self, role_id):
+        role_type = self._roles.get(role_id)
+        if not role_type:
+            raise ValueError('role_id')
+        return role_type()
+
     def track(self, employees, hours):
         print('Tracking Employee Productivity')
         print('==============================')
@@ -13,21 +27,32 @@ class ProductivitySystem:
             print()
 
 
+# Role classes implementation omitted
+
+_productivity_system = _ProductivitySystem()
+
+def get_role(role_id):
+    return _productivity_system.get_role(role_id)
+
+def track(employees, hours):
+    _productivity_system.track(employees, hours)
 class ManagerRole:
-    def work(self, hours):
+    def perform_duties(self, hours):
         return f'screams and yells for {hours} hours.'
 
 
 class SecretaryRole:
-    def work(self, hours):
+    def perform_duties(self, hours):
         return f'expends {hours} hours doing office paperwork.'
 
 
 class SalesRole:
-    def work(self, hours):
+    def perform_duties(self, hours):
         return f'expends {hours} hours on the phone.'
 
 
 class FactoryRole:
-    def work(self, hours):
+    def perform_duties(self, hours):
         return f'manufactures gadgets for {hours} hours.'
+
+
